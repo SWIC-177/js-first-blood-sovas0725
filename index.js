4886d7a12863ea7fdee0c3d4042ac6f80d1b0ad5
Shane Sova <shane.sova@student.swic.edu>
2024-02-21 22:46:35 -0600

    Fix bug in greet function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Function declaration that creates the string
-function greet(name){
-    return ("Hello, ", name)
+function greet(name) {
+    return ("Hello, " + name)
 }
 
 // Call the function and store the value in a variable
