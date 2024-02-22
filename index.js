8439a5891e8fa3d447d84a92de0bbbe6d23d2720
Shane Sova <shane.sova@student.swic.edu>
2024-02-21 22:54:20 -0600

    Rewrite greet function to use template literal

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Function declaration that creates the string
 function greet(name) {
-    return ("Hello, " + name)
+    return (`Hello, ${name}`)
 }
 
 // Call the function and store the value in a variable
