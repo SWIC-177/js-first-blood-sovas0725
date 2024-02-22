34b77fa85635e70746ddfd0b607d47b4ceea07c3
Shane Sova <shane.sova@student.swic.edu>
2024-02-21 22:41:22 -0600

    Add greeting variable

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// Function declaration
+// Function declaration that creates the string
 function greet(name){
     return ("Hello, ", name)
 }
+
+// Call the function and store the value in a variable
+const greeting = greet("Shane")
+
