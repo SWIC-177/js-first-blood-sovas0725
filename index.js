0920a8bff9d35dcb53c0ff735a62fb58de1126ed
Shane Sova <shane.sova@student.swic.edu>
2024-02-27 14:05:01 -0600

    Fix .gitignore ignoring .vscode directory

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Function declaration that creates the string
 function greet(name) {
-    return (`Hello, ${name}`)
+  return `Hello, ${name}`;
 }
 
 // Call the function and store the value in a variable
-const greeting = greet("Shane")
+const greeting = greet("Shane");
 
 // Print the variable to the console.
-console.log(greeting)
+console.log(greeting);
