462acbe4c0c9bd876a63bdadf9faee0ef70b76aa
Shane Sova <shane.sova@student.swic.edu>
2024-03-25 18:06:09 -0500

    Fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const person = {
   name: "John",
-  age: 17,
+  age: 18,
 };
 
 function verifyAdulthood(personName, personAge) {
@@ -9,6 +9,6 @@ function verifyAdulthood(personName, personAge) {
   return `You are not old enough to enter, ${personName}.`;
 }
 
-const adultMessage = verifyAdulthood(userName, userAge);
+const adultMessage = verifyAdulthood(person.name, person.age);
 
 console.log(adultMessage);
