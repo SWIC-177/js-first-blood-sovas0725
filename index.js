333c8210137f233565de159a127f06207a0f1f3c
Shane Sova <shane.sova@student.swic.edu>
2024-03-26 00:51:30 -0500

    Edit index.js

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const person = {
-  name: "John",
-  age: 18,
-};
+const people = [
+  {
+    name: "John",
+    age: 17,
+  },
+  {
+    name: "Jane",
+    age: 20,
+  },
+  {
+    name: "Mary",
+    age: 25,
+  },
+  {
+    name: "Peter",
+    age: 30,
+  },
+  {
+    name: "Paul",
+    age: 35,
+  },
+];
 
 function verifyAdulthood(personName, personAge) {
   if (personAge >= 18) return `Welcome, ${personName}!`;
@@ -9,6 +27,7 @@ function verifyAdulthood(personName, personAge) {
   return `You are not old enough to enter, ${personName}.`;
 }
 
-const adultMessage = verifyAdulthood(person.name, person.age);
-
-console.log(adultMessage);
+for (let i = 0; i <= people.length - 1; i += 1) {
+  const adultMessage = verifyAdulthood(people[i].name, people[i].age);
+  console.log(adultMessage);
+}
