32b2d526d8f8e396d0334c9a854735e15cb15906
Shane Sova <shane.sova@student.swic.edu>
2024-04-05 11:25:18 -0500

    Edit index.js to add numberAddition1 and numberAddition2 functions

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
-const people = [
-  {
-    name: "John",
-    age: 17,
-  },
-  {
-    name: "Jane",
-    age: 20,
-  },
-  {
-    name: "Mary",
-    age: 25,
-  },
-  {
-    name: "Peter",
-    age: 30,
-  },
-  {
-    name: "Paul",
-    age: 35,
-  },
-];
-
-function verifyAdulthood(personName, personAge) {
-  if (personAge >= 18) return `Welcome, ${personName}!`;
-
-  return `You are not old enough to enter, ${personName}.`;
+function numberAddition1(a, b) {
+  return a + b;
 }
-
-for (let i = 0; i <= people.length - 1; i += 1) {
-  const adultMessage = verifyAdulthood(people[i].name, people[i].age);
-  console.log(adultMessage);
-}
+const numberAddition2 = function numberAddition2(a, b) {
+  return a + b;
+};
